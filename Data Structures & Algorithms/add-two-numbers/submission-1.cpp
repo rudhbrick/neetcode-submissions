38db09838a51fe6b4a98cd64285e335b0252b715
rudhbrick <anirudhu07@gmail.com>
2026-06-22 10:51:30 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy=new ListNode(0);
+        ListNode* tail=dummy;
+        int carry=0;
+        while(l1||l2||carry){
+            int sum=carry;
+            if(l1){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            int digit=sum%10;
+            carry=sum/10;
+            tail->next=new ListNode(digit);
+            tail=tail->next;
+        }
+        ListNode* ans=dummy->next;
+        delete dummy;
+        return ans;
+    }
+};
