0e3f9259ba19cc633e3a2697d32da417d156ce55
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-08-06 23:45:20 +0530

    Add: linked-list-cycle-detection - submission-6

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode s=head,f=head;
+        while(f!=null&&f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s==f) return true;
+        }
+        return false;
+    }
+}
