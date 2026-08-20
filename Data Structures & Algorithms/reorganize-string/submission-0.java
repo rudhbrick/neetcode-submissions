a8f54edd8f5bacbdbf94e0d45b156578e2663695
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-08-20 13:37:19 +0530

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.java
@@ -0,0 +1,24 @@
+class Solution{
+    public String reorganizeString(String s){
+        int[] freq=new int[26];
+        for(char c:s.toCharArray()) freq[c-'a']++;
+        PriorityQueue<int[]> q=new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int i=0;i<26;i++){
+            if(freq[i]>0) q.offer(new int[]{i,freq[i]});
+        }
+        StringBuilder sb=new StringBuilder();
+        int prevChar=-1;
+        int prevFreq=0;
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int currChar=curr[0];
+            int currFreq=curr[1];
+            sb.append((char)('a'+currChar));
+            currFreq--;
+            if(prevFreq>0) q.offer(new int[]{prevChar,prevFreq});
+            prevChar=currChar;
+            prevFreq=currFreq;
+        }
+        return sb.length()==s.length()?sb.toString():"";
+    }
+}
