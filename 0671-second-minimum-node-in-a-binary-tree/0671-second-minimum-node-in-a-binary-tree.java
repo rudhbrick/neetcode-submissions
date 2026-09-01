5553fbec891dd56b8a753f3eeacaaaa4c1a0c55b
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-01 19:48:36 +0530

    Time: 0 ms (100%), Space: 43 MB (9.38%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -1,12 +1,10 @@
 class Solution{
     int second=-1;
-    public void dfs(TreeNode root,int min){
+    void dfs(TreeNode root,int min){
         if(root==null) return;
-        if(root.val>min){
-            if(second==-1||root.val<second) second=root.val;
-        }
-        dfs(root.right,min);
+        if(root.val>min) if(second==-1||root.val<second) second=root.val;
         dfs(root.left,min);
+        dfs(root.right,min);
     }
     public int findSecondMinimumValue(TreeNode root){
         int min=root.val;
