0f187eb2a293d7fc027a94c5eff7c7aac0e6c6a4
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-02 20:37:58 +0530

    Add: balanced-binary-tree - submission-6

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-6.java
@@ -0,0 +1,12 @@
+class Solution{
+    int dfs(TreeNode root){
+        if(root==null) return 0;
+        int leftHeight=dfs(root.left);
+        int rightHeight=dfs(root.right);
+        if(leftHeight==-1||rightHeight==-1||Math.abs(leftHeight-rightHeight)>1) return -1;
+        return 1+Math.max(leftHeight,rightHeight);
+    }
+    public boolean isBalanced(TreeNode root){
+        return dfs(root)!=-1;
+    }
+}
