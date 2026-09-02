99ee98bf3f33c4f1dfba213d3c65c6bd3e35b506
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-02 21:18:33 +0530

    Add: binary-tree-preorder-traversal - submission-2

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-2.java
@@ -0,0 +1,14 @@
+class Solution{
+    public List<Integer> preorderTraversal(TreeNode root){
+        Stack<TreeNode> st=new Stack<>();
+        List<Integer> result=new ArrayList<>();
+        if(root!=null) st.push(root);
+        while(!st.isEmpty()){
+            TreeNode node=st.pop();
+            result.add(node.val);
+            if(node.right!=null) st.push(node.right);
+            if(node.left!=null) st.push(node.left);
+        }
+        return result;
+    }
+}
