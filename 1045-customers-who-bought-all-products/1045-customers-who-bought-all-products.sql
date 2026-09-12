80f7b99d50ed421bd37170259352e5cd24df8f7a
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-12 22:38:38 +0530

    Time: 662 ms (40.12%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT c.customer_id FROM Customer c JOIN Product p ON c.product_key=p.product_key GROUP BY c.customer_id HAVING COUNT(DISTINCT p.product_key) = (SELECT COUNT(*) FROM Product);
