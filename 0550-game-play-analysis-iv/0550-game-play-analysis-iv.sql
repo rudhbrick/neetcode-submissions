801cf80cafffdb727a45076c12e731054074f4d9
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-12 19:15:34 +0530

    Time: 962 ms (12.83%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,1 @@
+SELECT ROUND(COUNT(*)/(SELECT COUNT(DISTINCT player_id) FROM ACTIVITY),2) AS fraction FROM (SELECT player_id,MIN(event_date) AS first_login FROM Activity GROUP BY player_id) a1 WHERE EXISTS (SELECT 1 FROM Activity a2 WHERE a2.player_id=a1.player_id AND a2.event_date=DATE_ADD(a1.first_login,INTERVAL 1 DAY));
