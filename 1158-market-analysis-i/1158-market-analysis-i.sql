14a40982cd1e6b388336e9f6f66bc89b8a82bc7b
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-14 17:16:09 +0530

    Time: 1860 ms (10.53%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id AS buyer_id,u.join_date,COUNT(
+    CASE
+        WHEN o.order_date BETWEEN '2019-01-01' AND '2019-12-31' THEN 1
+    END
+) AS orders_in_2019 FROM Users u LEFT JOIN Orders o ON u.user_id=o.buyer_id GROUP BY u.user_id,u.join_date;
