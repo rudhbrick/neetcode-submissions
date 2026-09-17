527393dd646ddc39ab9e7758233012ff97f0c0b5
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-17 14:44:38 +0530

    Time: 616 ms (53.28%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,1 +1,1 @@
-SELECT e.employee_id,e.department_id FROM Employee e JOIN (SELECT employee_id,COUNT(*) AS dept_count FROM Employee GROUP BY employee_id) d ON e.employee_id=d.employee_id WHERE e.primary_flag='Y' OR d.dept_count=1;
+(SELECT employee_id,department_id FROM Employee WHERE primary_flag='Y') UNION ALL (SELECT employee_id,department_id FROM Employee GROUP BY employee_id HAVING COUNT(department_id)=1);
