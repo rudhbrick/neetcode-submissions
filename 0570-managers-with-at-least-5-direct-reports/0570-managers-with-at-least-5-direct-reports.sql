6c022186c4e60ed09a2a4e41a168eee3f39763d7
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-18 23:10:51 +0530

    Time: 373 ms (63.33%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,1 +1,1 @@
-SELECT e.name FROM Employee e JOIN(SELECT managerId FROM Employee GROUP BY managerId HAVING COUNT(id)>=5) m ON e.id=m.managerId;
+SELECT e.name FROM Employee e JOIN (SELECT managerId FROM Employee GROUP BY managerId HAVING COUNT(id)>=5) m ON e.id=m.managerId;
