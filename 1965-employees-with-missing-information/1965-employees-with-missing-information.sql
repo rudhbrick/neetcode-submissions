9cb68c2ccc2b900f357924ca6d3ab725837463b0
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-18 14:38:49 +0530

    Time: 555 ms (90.83%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,1 @@
+(SELECT e.employee_id FROM Employees e LEFT JOIN Salaries s ON e.employee_id=s.employee_id WHERE e.name IS NULL OR s.salary IS NULL) UNION ALL (SELECT s.employee_id FROM Employees e RIGHT JOIN Salaries s ON e.employee_id=s.employee_id WHERE e.name IS NULL OR s.salary IS NULL) ORDER BY employee_id;
