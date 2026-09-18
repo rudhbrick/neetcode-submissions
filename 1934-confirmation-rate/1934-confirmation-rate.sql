79a34c398f20dcf894a509d381429f1af2b7d74c
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-18 12:07:08 +0530

    Time: 722 ms (63.49%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,1 @@
+SELECT s.user_id,ROUND(COALESCE(COUNT(CASE WHEN action='confirmed' THEN 1 END)*1.0/NULLIF(COUNT(c.action),0),0),2) AS confirmation_rate FROM Signups s LEFT JOIN Confirmations c ON s.user_id=c.user_id GROUP BY s.user_id;
