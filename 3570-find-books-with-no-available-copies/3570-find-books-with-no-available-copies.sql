d5bd3827ffd7d720fcdd2a3c2e55e2c72f2a7c50
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-20 18:41:27 +0530

    Time: 524 ms (89%), Space: 0B (100%) - LeetHub

---
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -0,0 +1,1 @@
+SELECT b.book_id,b.title,b.author,b.genre,b.publication_year,br.current_borrowers FROM library_books b LEFT JOIN (SELECT book_id,COUNT(*) AS current_borrowers FROM borrowing_records WHERE return_date IS NULL GROUP BY book_id) br ON b.book_id=br.book_id WHERE br.current_borrowers=total_copies AND br.current_borrowers>0 ORDER BY br.current_borrowers DESC,b.title ASC;
