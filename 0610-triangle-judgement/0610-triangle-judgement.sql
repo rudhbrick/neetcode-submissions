4d198120d5037b52473f3ec55e1e130689636af1
Anirudh Upadhyayula <anirudhu07@gmail.com>
2026-09-20 22:06:24 +0530

    Time: 471 ms (7.97%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,9 +1,6 @@
-SELECT x, y, z,
-    CASE
-        WHEN x + y > z
-         AND x + z > y
-         AND y + z > x
-        THEN 'Yes'
-        ELSE 'No'
-    END AS triangle
-FROM Triangle;
+SELECT x,y,z,CASE
+                WHEN x+y>z
+                AND y+z>x
+                AND z+x>y THEN 'Yes'
+                ELSE 'No'
+            END AS triangle FROM Triangle;
